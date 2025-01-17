b84329edc4be901f4acb47aefe9afaa3cc443efb
Vladimir <V_Sukhov@mail.ru>
2025-01-17 16:36:09 +0300

    abstract formula concept base realization

--- avro_fapi.py
+++ avro_fapi.py
@@ -11,10 +11,9 @@ app = FastAPI()
 # Модель для входных данных
 class TransformRequest(BaseModel):
     inputFileName: str  # Имя входного файла
-    operation: str      # Тип операции: "sum"
+    outputFileName: str # Имя выходного файла
     resultColumnName: str  # Имя результирующего столбца
-    column1: str        # Первый столбец для сложения
-    column2: str        # Второй столбец для сложения
+    formula: str         # Формула для вычисления нового столбца
 
 # Модель для выходных данных
 class TransformResponse(BaseModel):
@@ -30,30 +29,24 @@ class TransformResponse(BaseModel):
           tags=["Data Transformation"])
 def transform(request: TransformRequest):
     input_file = request.inputFileName
-    operation = request.operation
     result_column = request.resultColumnName
-    column1 = request.column1
-    column2 = request.column2
-    output_file = "output.avro"  # Имя выходного файла (по умолчанию)
+    output_file = request.outputFileName
+    formula = request.formula
 
     try:
+        code = compile(source=formula, filename='', mode='eval')
+
         # Читаем данные из входного файла Avro
         with open(input_file, "rb") as infile:
             reader = fastavro.reader(infile)
             data = [record for record in reader]
             schema = reader.schema
 
-        # Обрабатываем данные в зависимости от типа операции
-        if operation == "sum":  # Пример: сложение двух указанных столбцов
-            for record in data:
-                if column1 in record and column2 in record:
-                    record[result_column] = record[column1] + record[column2]
-                else:
-                    raise ValueError(f"Ожидаются столбцы '{column1}' и '{column2}' для операции 'sum'.")
-            schema["fields"].append({"name": result_column, "type": "int"})
-
-        else:
-            raise ValueError(f"Неизвестная операция: {operation}")
+        # Применяем формулу
+        for col in data:
+            col[result_column] = eval(code)
+        
+        schema["fields"].append({"name": result_column, "type": "int"})
 
         # Записываем данные в выходной файл Avro
         with open(output_file, "wb") as outfile:
